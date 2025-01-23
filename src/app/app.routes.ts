8739d9abc7f2b54b0c65df8c2b9cbc03555708da
PratikMehta <premmehta7607@gmail.com>
2025-01-23 23:14:25 +0530

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,5 +7,4 @@ export const routes: Routes = [
     {path: '', component: IndexComponent},
     {path: 'resume-maker', component: ResumeMakerComponent},
     {path: 'practical-page', component: PracticalPageComponent},
-    { path: 'practical-page/resume-maker', component: ResumeMakerComponent }
 ];
